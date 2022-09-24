ba64a0bd83fd6bd84133d0db122564785ec2c055
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2022-09-24 15:03:43 +0300

    1.10

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -21,15 +21,28 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good*1 + neutral*0 + bad*-1)/all}</p>
-      <p>positive {good/all*100} %</p>
-    </div>
+      <StatisticLine text="good" value ={good} />
+      <StatisticLine text="neutral" value ={neutral} />
+      <StatisticLine text="bad" value ={bad} />
+      <StatisticLine text="all" value ={all} />
+      <StatisticLine text="average" value ={(good*1 + neutral*0 + bad*-1)/all} />
+      <StatisticLine text="positive" value ={good/all*100 + " %"} />
+      </div>
   )
 }
+
+const StatisticLine = (props) => (
+  <table>
+    <tbody>
+      <tr>
+        <td>{props.text} </td>
+        <td>{props.value} </td>
+      </tr>
+    </tbody>
+  </table>
+)
+
+
 
 const App = () => {
   // tallenna napit omaan tilaansa
