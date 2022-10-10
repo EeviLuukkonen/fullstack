3f23a3231a029f20c1aa2df1d565d3852d17be84
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2022-10-10 15:42:09 +0300

    2.13-2.14

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,19 +1,36 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Display = ({ matches }) => {
-    if (matches.length > 10) {
-      return (<div>Too many matches, specify another filter</div>)
-    } else if (matches.length === 1) {
-      return (<CountryInfo country={matches[0]}/>)
-    } else {
-      return (<div>{matches.map(country => 
-        <p key={country.name.common}>{country.name.common}</p>)}</div>)
-    }
+const Display = ({ matches, setInput }) => {
+  const handleClick = (country) => {
+    setInput(country)
+  }
+
+  if (matches.length > 10) {
+    return (<div>Too many matches, specify another filter</div>)
+  } else if (matches.length === 1) {
+    return (
+      <div>
+        <CountryInfo country={matches[0]}/>
+        <Weather capital={matches[0].capital}/>
+      </div>
+      )
+  } else {
+    return (
+      <div>
+      {matches.map(country => 
+        <p key={country.name.common}>
+          {country.name.common}
+          <button onClick={() => handleClick(country.name.common)}>info</button>
+        </p>
+      )}
+    </div>
+    )
+  }
 }
 
 const CountryInfo = ({ country }) => {
-  console.log(country.languages)
+  console.log(country)
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -32,20 +49,51 @@ const CountryInfo = ({ country }) => {
   )
 }
 
+const Weather = ({ capital }) => {
+  const [weather, setWeather] = useState()
+  const api_key = process.env.REACT_APP_API_KEY
+  console.log(weather)
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital[0]}&appid=${api_key}&units=metric`)
+      .then(response => {
+        console.log(`weather of ${capital} fetched`)
+        setWeather(response.data)
+        console.log(weather, response.data)
+      })
+  }, [])
+
+  if (!weather) {
+    return null
+  } else {
+    return (
+      <div>
+        <h2>Weather in {capital}</h2>
+          <p>
+            temperature {weather.main.temp} Celsius
+          </p>
+          <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} width="150px" height="auto"></img>
+          <p>
+            wind {weather.wind.speed} m/s
+          </p>
+      </div>
+    )
+  }
+}
+
 const App = () => {
   const [input, setInput] = useState('')
   const [data, setData] = useState([])
 
   const handleChange = (event) => {
     setInput(event.target.value)
-    console.log(event.target.value)
   }
 
   const countriesToShow = data.filter(country => 
     country.name.common.toLowerCase().includes(input.toLowerCase()))
 
   useEffect(() => {
-    console.log('effect')
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
@@ -62,7 +110,7 @@ const App = () => {
         find countries
         <input value={input} onChange={handleChange} />
       </form>
-      <Display matches={countriesToShow}/>
+      <Display matches={countriesToShow} setInput={setInput}/>
     </div>
   )
 }
