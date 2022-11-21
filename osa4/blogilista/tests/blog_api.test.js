5f357c143e3ce05cbfcf9424179c1347188a62ab
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2022-11-21 16:52:16 +0200

    4.11

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -68,6 +68,26 @@ test('a valid blog can be added', async () => {
     expect(titles).toContain('testiblogi')
 })
 
+test('if likes isn`t defined it`s value is 0', async () => {
+    const newBlog = {
+        "author": "testNoLikes",
+        "title": "testiblogi",
+        "url": "www.jee.com"
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+    const nolikes = response.body.find(r => r.author === "testNoLikes")
+    console.log(nolikes)
+
+    expect(nolikes.likes).toBe(0)
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
