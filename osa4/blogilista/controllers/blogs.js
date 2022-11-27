a1e25bc33d67915e497ebbf090e7ad6c112dd46a
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2022-11-27 16:37:13 +0200

    4.17

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,10 +1,12 @@
 const blogsRouter = require('express').Router()
 const { request, response } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+      .find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
@@ -16,21 +18,26 @@ blogsRouter.post('/', async (request, response) => {
         error: 'missing title'
     })
   }
-
   if (!body.url) {
     return response.status(400).json({
         error: 'missing url'
     })
   }
+
+  const user = await User.findById(body.user)
   
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
+    user: user._id,
     likes: body.likes || 0
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
