270bebec676d39a9fd55579ce98cd59280c510ec
EeviLuukkonen <eevi.luukkonen@gmail.com>
2023-01-10 14:51:59 +0200

    5.17 and cypress configurations

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -29,7 +29,7 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    user: user._id,
+    user: user,
     likes: body.likes || 0
   })
 
