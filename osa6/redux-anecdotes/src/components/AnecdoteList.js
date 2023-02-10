cf97912ec3117ae650ae68a7cd737706763ed97c
EeviLuukkonen <eevi.luukkonen@gmail.com>
2023-02-10 12:40:56 +0200

    6.9

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,13 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = (props) => {
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+  const anecdotes = useSelector(state => {
+    if ( state.filter === '' ) {
+      return state.anecdotes.sort((a, b) => b.votes - a.votes)
+    }
+    return state.anecdotes.filter(anecdote => 
+      anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,9 +19,9 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
-  console.log('action', action)
+  console.log('ACTION', action)
   switch(action.type) {
     case 'NEW_ANECDOTE':
       const object = asObject(action.data)
@@ -54,4 +54,4 @@ export const voteAnecdote = (id) => {
   }
 }
 
-export default reducer
+export default anecdoteReducer
