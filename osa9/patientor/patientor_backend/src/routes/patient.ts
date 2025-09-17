5186830a967d55a31dd970f8eb479ac7f16f8bc4
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2025-09-17 13:00:25 +0300

    9.10-9.11

---
+++ osa9/patientor/patientor_backend/src/routes/patient.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import patientService from '../services/patientService';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+    console.log("fetching patients");
+    res.send(patientService.getNonSensitivePatients());
+  });
+
+export default router;
