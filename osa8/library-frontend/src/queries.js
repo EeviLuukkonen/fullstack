3209a43a1b6fbb30c7c550b05506ae6e831e0dad
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2025-10-03 14:46:34 +0300

    8.8-8.10

---
+++ osa8/library-frontend/src/queries.js
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+        name
+        born
+        bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+        title
+        author
+        published
+    }
+  }
+`
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!, $publishedInt: Int!, $author: String!, $genres: [String!]!) {
+    addBook(
+      title: $title,
+      published: $publishedInt,
+      author: $author,
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`
